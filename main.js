662ef26fec868e0333987e9e80a3a0ab3203b1b0
jeanvitor10 <111467996+jeanvitor10@users.noreply.github.com>
2023-10-20 10:57:09 -0300

    kk

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 //DECLARAÇÃO/criação da função tocaSomPom
 function tocaSomPom(){                             
-    document.querySelector('#som_trcla_pom').play();
+    document.querySelector('#som_tecla_pom').play();
 }
 //chamada ou invocação da função tocaSomPom ao clicar no botão
 document.querySelector('tecla_pom').onclick = tocaSomPom;
 
 //DECLARAÇÃO/criação da função tocaSomPom
 function tocaSomPom(){                             
-    document.querySelector('#som_trcla_pom').play();
+    document.querySelector('#som_tecla_pom').play();
 }
 //chamada ou invocação da função tocaSomPom ao clicar no botão
 document.querySelector('tecla_pom').onclick = tocaSomPom;
